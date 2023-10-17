93f5279d443b936b81a0acf482f366f3abf2727c
Kendall Madrigal <kendallmadrigal14@gmail.com>
2023-10-17 15:58:16 -0600

    Component template

---
+++ src/components/projectComponent.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const ProjectComponent = () => {
+    return (
+        <div class="card">
+            <div class="image"></div>
+            <div class="img-overlay">
+            </div>
+            <div class="content">
+                <div class="content-text">
+                    <span class="project-name">Titulo</span>
+                    <div class="content-body">
+                        <span class="project-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita quod vel ducimus. Itaque, quam repellendus. Doloremque commodi cupiditate expedita id laudantium qui minima at, a ullam provident animi unde sint.</span>
+                    </div>
+
+                    <span class="categories">
+                        <a class="categories-btn" href="#">Español</a>
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectComponent;
